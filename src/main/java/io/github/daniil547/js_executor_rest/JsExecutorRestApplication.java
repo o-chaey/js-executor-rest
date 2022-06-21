2df123f65f8cb0d1a5c6d4349fac04a49cf05e1d
Daniil547 <72149318+Daniil547@users.noreply.github.com>
2022-06-21 21:32:22 +0300

    Initial commit

---
+++ src/main/java/io/github/daniil547/js_executor_rest/JsExecutorRestApplication.java
@@ -0,0 +1,13 @@
+package io.github.daniil547.js_executor_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsExecutorRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsExecutorRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/daniil547/js_executor_rest/JsExecutorRestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.daniil547.js_executor_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsExecutorRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
