d6eb2f9ac24b5b4e7aa04928af0f60319075f3d5
Daniil547 <72149318+Daniil547@users.noreply.github.com>
2022-07-14 10:16:48 +0100

    Add Swagger and OpenAPI docs

--- src/main/java/io/github/daniil547/js_executor_rest/dtos/NoInputTaskDto.java
+++ src/main/java/io/github/daniil547/js_executor_rest/dtos/NoInputTaskDto.java
@@ -1,6 +1,7 @@
 package io.github.daniil547.js_executor_rest.dtos;
 
-import io.github.daniil547.js_executor_rest.domain.LanguageTask;
+import io.swagger.v3.oas.annotations.media.Schema;
 
+@Schema(name = "Task")
 public record NoInputTaskDto(String source) {
 }
--- src/main/java/io/github/daniil547/js_executor_rest/dtos/PatchTaskDto.java
+++ src/main/java/io/github/daniil547/js_executor_rest/dtos/PatchTaskDto.java
@@ -1,6 +1,8 @@
 package io.github.daniil547.js_executor_rest.dtos;
 
 import io.github.daniil547.js_executor_rest.domain.LanguageTask;
+import io.swagger.v3.oas.annotations.media.Schema;
 
+@Schema(title = "PatchTask")
 public record PatchTaskDto(LanguageTask.Status status) {
 }
