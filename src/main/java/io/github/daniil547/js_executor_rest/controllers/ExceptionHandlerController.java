e1146d3a4271085db1326a977061b79a21f54e32
Daniil547 <72149318+Daniil547@users.noreply.github.com>
2022-08-24 16:17:19 +0100

    Fix 500 status code being returned on auth (use 401 and 403 instead)

--- src/main/java/io/github/daniil547/js_executor_rest/controllers/ExceptionHandlerController.java
+++ src/main/java/io/github/daniil547/js_executor_rest/controllers/ExceptionHandlerController.java
@@ -3,17 +3,52 @@ package io.github.daniil547.js_executor_rest.controllers;
 import cz.jirutka.rsql.parser.RSQLParserException;
 import io.github.daniil547.js_executor_rest.util.HttpUtils;
 import org.graalvm.polyglot.PolyglotException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.NativeWebRequest;
 import org.zalando.problem.Problem;
 import org.zalando.problem.Status;
 import org.zalando.problem.ThrowableProblem;
 import org.zalando.problem.spring.web.advice.ProblemHandling;
 
+import javax.annotation.ParametersAreNonnullByDefault;
+
+import static org.zalando.problem.Status.UNAUTHORIZED;
+
 @SuppressWarnings("unused")
 @RestControllerAdvice
 public class ExceptionHandlerController implements ProblemHandling {
+
+    @Override
+    @ParametersAreNonnullByDefault
+    public ResponseEntity<Problem> create(final Throwable throwable, final Problem problem,
+                                          final NativeWebRequest request) {
+        HttpHeaders headers = new HttpHeaders();
+        if (problem.getStatus() == UNAUTHORIZED) {
+            headers.add(HttpHeaders.WWW_AUTHENTICATE,
+                        "Bearer realm=\"JsExecutor\" " +
+                        "scope=\"openid swagger\"" +
+                        "error=\"invalid_token\"," +
+                        "error_description=\"The access token expired\"");
+
+        }
+        return ProblemHandling.super.create(throwable, problem, request, headers);
+    }
+
+    @Override
+    @ParametersAreNonnullByDefault
+    public ThrowableProblem toProblem(Throwable throwable) {
+        if (throwable instanceof AuthenticationException) {
+            return ProblemHandling.super.toProblem(throwable, UNAUTHORIZED);
+        } else if (throwable instanceof AccessDeniedException) {
+            return ProblemHandling.super.toProblem(throwable, Status.FORBIDDEN);
+        }
+        return ProblemHandling.super.toProblem(throwable);
+    }
 
     @ExceptionHandler
     public ResponseEntity<ThrowableProblem> toProblem(RSQLParserException exc) {
